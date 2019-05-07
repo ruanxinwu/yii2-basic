ed19c1ead895169e66f1a33b90b91a9023a70a60
ruanxinwu <ruan.xw@jiahongnet.com>
2019-05-07 12:49:41 +0800

    ori

--- modules/user/controllers/UserController.php
+++ modules/user/controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
 {
     public function actionIndex()
     {
-        echo 'ruanxinwu';
+        echo 'ruanxinwu' . 'sdddd';
     }
 
     /**
---
+++ themes/actions/ErrorAction.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruanxinwu
+ * Date: 2019/5/7
+ * Time: 10:54
+ */
+namespace app\themes\actions;
+use yii\base\Action;
+class ErrorAction extends Action
+{
+    public function run()
+    {
+        echo 23;
+    }
+}
