485ccfe10ba949f90d2bd7de2274e0c18d7f6fb9
ruanxw <ruanxinwu92@163.com>
2019-11-21 22:23:37 +0800

    dd

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=mysail_seller',
     'username' => 'root',
-    'password' => '012345ssss6',
+    'password' => '012345sssssss6',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
